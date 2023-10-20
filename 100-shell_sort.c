023219650b9a46f7b12c7c9dfb83aa9104f6cf58
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-20 11:46:11 +0100

    Task 4: fix errors

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,26 +8,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, gap = 1;
+	size_t i, j, interval = 1;
 	int temp;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
+	while (interval < size / 3)
+		interval = interval * 3 + 1;
 
-	while (gap > 0)
+	while (interval > 0)
 	{
-		for (i = gap; i < size; i++)
+		if (interval == 1)
+			print_array(array, size);
+		for (i = interval; i < size; i++)
 		{
 			temp = array[i];
 			j = i;
-			while (j >= gap && array[j - gap] > temp)
+			while (j >= interval && array[j - interval] > temp)
 			{
-				array[j] = array[j - gap];
-				j -= gap;
+				array[j] = array[j - interval];
+				j -= interval;
 			}
 			array[j] = temp;
 		}
-		print_array(array, size);
-		gap = (gap - 1) / 3;
+		interval = (interval - 1) / 3;
 	}
+	print_array(array, size);
 }
